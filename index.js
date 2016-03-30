bbdbd96dde7e898e6d169ceabf22b00cc12d3c0f
lukesUbuntu <luke@hardiman.co.nz>
2016-03-30 13:48:21 +1300

    Updated 1.01 resolved status codes not been applied

--- index.js
+++ index.js
@@ -6,46 +6,54 @@
 
 var api = {
     _res: false,
-    _req : false,
+    _req: false,
     _headers: [],
     headers: {
         add: function (header, value) {
+            //if (!api._res.headersSent)
             api._headers[header] = value
         }
     },
-    send: function (data, success,statusCode) {
+    send: function (data, success) {
 
         api.success = typeof success == "undefined" ? true : success;
+        var statusCode = (typeof success == "number") ? parseInt(statusCode) : (api.success == true ? 200 : 400);
 
-        if (api._res == false) {
+        if (api._res == false)
             return console.dir("apihelper Can't send response, api.res not set");
-        }
-        //apend headers
+
+
+        //Issue Can't set headers after they are sent
         for (var header in api._headers)
-            api._res.setHeader(header,api._headers[header]);
+            api._res.setHeader(header, api._headers[header]);
 
 
+        try {
+            api._res.status(statusCode).json({
+                'data': data,
+                'success': api.success
+            });
+        } catch (e) {
+        }
 
 
-        api._res.status((typeof statusCode == "number") ? parseInt(statusCode) : (api.success == true) ? 200 : 400).send(JSON.stringify({
-            'data': data,
-            'success': api.success
-        }));
     },
-    getVars : function(pram){
+    getVars: function (pram) {
         return (typeof pram == "string") ? api._req.params[pram] : api._req.params;
     },
-    getRequest : function(){
+    getRequest: function () {
         return api._res;
     },
-    getResponse : function(){
+    getResponse: function () {
         return api._req;
     },
     helper: function (req, res, next) {
-        api.headers.add('Content-Type', 'application/json');
+        //api.headers.add('Content-Type', 'application/json');
         api._res = res;
         api._req = req;
         next();
+
+
     }
 };
 
